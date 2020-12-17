cf03bc4f6a9d70c58213249f397e1a21d1ceffee
Uzias Rivera <uziasrivera@gmail.com>
2020-12-17 14:59:59 -0800

    removed proxy

--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -20,7 +20,7 @@ const Games: React.FC = () => {
     const [filterInput, setFilterInput] = useState<string>("")
 
     useEffect(() => {
-        axios.get<Game[]>('api/games', { headers: { 'Content-Type': 'application/json' } })
+        axios.get<Game[]>('https://www.freetogame.com/api/games', { headers: { 'Content-Type': 'application/json' } })
             .then(res => setGame(res.data))
             .catch(err => console.log(err))
     }, [])
