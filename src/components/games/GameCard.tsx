9b5df65d4f90e8415ca90a711be9b9c7c0a9adeb
Uzias Rivera <uziasrivera@gmail.com>
2020-12-16 20:39:18 -0800

    rendering all games with pages

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -1,13 +1,59 @@
-import React, { } from "react"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Game from "./index"
 
+const useStyles = makeStyles({
+    root: {
+        width: 345,
+        margin: "20px"
+    },
+    media: {
+        height: 140,
+    },
+});
+
 const GameCard: React.FC<Game> = (game) => {
-    console.log("this is game", game)
+    const classes = useStyles();
+
+    const learnMoreLink = (title: string): string => {
+        const baseUrl = `https://www.google.com/search?&q=`
+        return baseUrl + title.split(" ").join("+")
+    }
+
     return (
-        <>
-            <p></p>
-        </>
-    )
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={game.thumbnail}
+                    title={game.title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {game.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {game.short_description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+        </Button>
+                <Button size="small" color="primary">
+                    <a style={{color:"#3f51b5", textDecoration: "none"}} href={learnMoreLink(game.title)}>Learn More</a>
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
 
 export default GameCard
--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -1,30 +1,19 @@
 import React, { useState, useEffect, useRef } from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
 import axios from "axios"
 import Game from "./index"
 import GameCard from "./GameCard"
+import TextField from '@material-ui/core/TextField';
 import "./index.css"
-
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-interface Props {
-    text: string;
-    ok?: boolean; // props is optional if ?
-    i: number;
-    fn: (bob: string) => string;
-    obj: {
-        f1: string
-    },
-    person: Person;
-}
 
 const Games: React.FC = ({ }) => {
 
-    const [loadByHundred, setLoadByHundred] = useState<number>(100)
+    const [loadPerPage, setLoadPerPage] = useState<number>(24)
     const [games, setGame] = useState<Game[]>([])
     const inputRef = useRef<HTMLInputElement>(null);
+    const [currentPage, setCurrentPage] = useState<number>(1)
+    const TotalPages: number = Math.ceil(games.length / loadPerPage)
 
     useEffect(() => {
         // axios.get<Game[]>('api/games', { headers: { 'Content-Type': 'application/json' } })
@@ -33,20 +22,21 @@ const Games: React.FC = ({ }) => {
     }, [])
 
     const loadGames = (): Game[] => {
-        return games.slice(0, loadByHundred)
-    }
-
-    const incrementLoad = (): void => {
-        setLoadByHundred(loadByHundred + 100)
+        return games.slice((currentPage - 1) * loadPerPage, loadPerPage * currentPage)
     }
 
     return (
         <>
-            <input ref={inputRef} onChange={(event: React.ChangeEvent<HTMLInputElement>) => 1} />
+            {/* <input ref={inputRef} onChange={(event: React.ChangeEvent<HTMLInputElement>) => 1} /> */}
+            <div className="inputFilterWrap">
+                <TextField style={{width:"80%"}} id="outlined-basic" label="Search By Name" variant="outlined" />
+            </div>
             <div className="gameCardWrap">
                 {loadGames().map<React.FunctionComponentElement<any>>(game => <GameCard key={game.id} {...game} />)}
             </div>
-            <button onClick={incrementLoad}>View More</button>
+            <div className="paginationWrap">
+                <Pagination count={TotalPages} onChange={(event: React.ChangeEvent<unknown>, page: number) => { setCurrentPage(page) }} variant="outlined" shape="rounded" />
+            </div>
         </>
     )
 }
